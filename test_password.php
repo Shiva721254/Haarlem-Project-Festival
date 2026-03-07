98028d67a696a33e1a70517e9378b3f4b8f19133
Shiva Lamichhane <721254@student.inholland.nl>
2026-03-07 23:30:46 +0100

    chore: remove unused test files

--- test_password.php
+++
@@ -1,6 +0,0 @@
-<?php
-$hash = '$2y$12$48/mAMTpsHZ0vcFitlPY9O3Gk.nnYHt9ZNA/ULGtzKYMdrUZQMJUW';
-$password = 'Admin123!';
-var_dump(['hash' => $hash, 'password' => $password]);
-echo password_verify($password, $hash) ? 'VERIFIED!' : 'FAILED!';
-echo "\n";
